655b3995d28f1d6b1c7e90a4254ceef7d8ea2a31
samira saeid <samirasaeid264@gmail.com>
2022-08-27 15:18:12 +0200

    adding faculty

--- example.c
+++ example.c
@@ -7,6 +7,7 @@ int main(void){
       print_my_name();
       print_my_age();
       printf_my_collage();
+      print_my_faculty();
 
     return 0;
 }
--- func.c
+++ func.c
@@ -5,13 +5,16 @@
 
 
 void print_my_name(void){
-    printf("My name is Samira\n");
+    printf("My name is Sameera\n");
 
     return;
 }
 void print_my_age(void){
-    printf("My age is 24\n");
+    printf("My age is 25\n");
 }
 void printf_my_collage(void){
     printf("My collage is HTI \n");
 }
+ void print_my_faculty(void){
+    printf ("My faculty is Engineering\n");
+ }
