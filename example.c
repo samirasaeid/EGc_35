29d3c19f213ee9ad1e01cb80c878391d03d67e6f
samira saeid <samirasaeid264@gmail.com>
2022-08-27 13:22:04 +0200

    adding files

---
+++ example.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"example.h"
+void print_my_name(void);
+
+int main(void){
+
+      print_my_name();
+
+    return 0;
+}
---
+++ func.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"example.h"
+
+
+
+
+void print_my_name(void){
+    printf("My name is Samira\n");
+
+    return;
+}
